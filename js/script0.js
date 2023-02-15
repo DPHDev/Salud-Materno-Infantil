910bf1532f9f7bf19ccee40699486306010850c0
DanielPerdomoDev <dfperdomodev@gmail.com>
2023-02-14 21:46:57 -0500

    menu hamburger responsive

--- js/script0.js
+++ js/script0.js
@@ -11,4 +11,12 @@
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
     }
-  })();
+})();
+  
+const menuHamburger = document.querySelector(".menu_icon");
+const navLinks = document.querySelector(".navbar");
+
+menuHamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu")
+}
+)
